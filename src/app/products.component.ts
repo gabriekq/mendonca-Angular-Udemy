213daac18de484d8a50343dded246ed0b31d5c99
gabriekq <gabrielmendona43@yahoo.com>
2022-08-01 12:22:13 -0300

    part-13

---
+++ src/app/products.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-products',
+    templateUrl: './products.component.html'
+})
+export class ProductsComponent{
+
+    productName = 'A Book';
+
+    isDisabled = true;
+
+
+     constructor(){
+        setTimeout( ()=>{
+         // this.productName = 'A Tree';
+          this.isDisabled = false;
+        },3000 )
+     }
+
+
+
+}
